d515ded98004c90e3e438edc451f1e3222b0a442
uxster <hello@uxster.nl>
2017-06-20 15:51:44 +0200

    the first jQuery is implemented woooo

--- includes/jQuery.js
+++ includes/jQuery.js
@@ -1,8 +1,15 @@
-$(document).ready(function() {			//check to see if all the elements are loaded
-	$('#welcome').click(function() {	//lookout for an element with the 'welcome' ID
-		$('img').hide();				//when clicked, images will hide 
-		$('body').css({					//and the background-color of body will change to #ffffff
-			"background-color": "#FFFFFF";	//access this element of the css of body
-		})
+$(document).ready(function() {
+
+	$("#pokemon").click(function() {	
+		$("img").hide();				 
 	})
+
+	// $("#pokemon").mouseEnter(function() {
+	// 	$("img").fadeIn();
+	// })
+
+	$("button").click(function() {
+		$("img").show("slow");
+	})
+
 });
