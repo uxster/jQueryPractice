40c3cd268885515471ab764d0ef6a1a46f052ca6
uxster <hello@uxster.nl>
2017-06-23 10:49:51 +0200

    Finishing touches on the assignment made

--- includes/jQuery.js
+++ includes/jQuery.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
 
-	$("#pokemon").click(function() {	
-		$("img").hide();				 
-	})
+	$( ".pokeball" ).hide();
 
-	// $("#pokemon").mouseEnter(function() {
-	// 	$("img").fadeIn();
-	// })
+	$( "#pokemon" ).click(function() {	
+		$( this ).fadeOut( 100 );
+		$( ".pokeball" ).fadeIn();		 
+	});
 
-	$("button").click(function() {
-		$("img").show("slow");
-	})
+	$( "#release" ).click(function() {
+		$( "#pokemon" ).show();
+		$( ".pokeball" ).hide();
+	});
+
+	$( "#clean img" ).hover(function() {
+  		$( this ).fadeOut( 2000 );
+	});
+
+	$( "#messy" ).click(function() {
+		$( "#clean img" ).fadeIn( 2000 );
+	});
+
+	$( "#hideandseek" ).mouseover(function() {
+    	$( "#jigglyimage" ).slideToggle( "slow", function(){
+    	} );
+	});
 
 });
